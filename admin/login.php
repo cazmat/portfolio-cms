9d9cebfaffab666ffa5c16e07cc4a8ac9524da51
cazmat <93198094+cazmat@users.noreply.github.com>
2025-12-15 21:52:40 -0600

    Remove default credentials

--- admin/login.php
+++ admin/login.php
@@ -74,12 +74,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                             <a href="../index.php" class="text-muted small">← Back to Portfolio</a>
                         </div>
                         
-                        <div class="alert alert-info mt-4 small">
-                            <strong>Default credentials:</strong><br>
-                            Username: admin<br>
-                            Password: admin123<br>
-                            <em>Please change these after first login!</em>
-                        </div>
                     </div>
                 </div>
             </div>
